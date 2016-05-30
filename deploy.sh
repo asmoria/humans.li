9a332db95cda9ad904394e7e06f549689f21ed2f
Christian Decker <decker.christian@gmail.com>
2016-05-30 19:22:55 +0200

    Added domain CNAME.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ VERSION=$(git rev-parse HEAD)
 cd public
 git init
 git remote add origin git@github.com:asmoria/humans.li.git
+echo humans.li > CNAME
 git add -A
 git commit -m "Deploying version $VERSION"
 git push -f origin master:gh-pages
